22dc0d2187b0135102a0acc992347ce0567b0e80
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-12-21 16:25:50 +0200

    feat: implement IndexView; base template structure; index template; and base static

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.db.models import Count
+from django.views import generic
+from .models import Topic, Redactor, Newspaper
 
-# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = "newspaper/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["num_topics"] = Topic.objects.count()
+        context["num_redactors"] = Redactor.objects.count()
+        context["num_newspapers"] = Newspaper.objects.count()
+
+        context["latest_newspapers"] = (
+            Newspaper.objects
+            .only("title", "published_date")
+            .prefetch_related("topics")
+            .order_by("-published_date")[:5]
+        )
+        context["top_redactors"] = (
+            Redactor.objects
+            .only("username", "first_name", "last_name")
+            .annotate(num_papers=Count("newspapers"))
+            .order_by("-num_papers")[:3]
+        )
+
+        return context
