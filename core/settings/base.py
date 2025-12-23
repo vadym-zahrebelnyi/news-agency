59b7e7ba57f63bf2a1f447876b2a04f3629259be
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-12-23 21:55:51 +0200

    fix: add render external hostname

--- core/settings/base.py
+++ core/settings/base.py
@@ -7,6 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 load_dotenv(BASE_DIR / ".env")
 
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
